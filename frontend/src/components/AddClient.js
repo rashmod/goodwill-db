1375c9a9d4136b3288c218e20ecd620f8bb100e1
Bhushan Alhat <alhatbhushan@gmail.com>
2023-02-03 00:19:03 +0530

    added conditional inputs and new property type field

--- frontend/src/components/AddClient.js
+++ frontend/src/components/AddClient.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 
 const AddClient = () => {
 	// * when saving to backend check property key
+	// todo make form input conditional render based on property type input
 
 	const [formData, setFormData] = useState({
 		clientName: '',
 		mobile: '',
 		address: '',
+		propertyType: '',
 		clientType: '',
 		rentParty: '',
 		saleParty: '',
@@ -44,6 +46,21 @@ const AddClient = () => {
 				setFormData({
 					...formData,
 					[e.target.name]: Number(e.target.value),
+				});
+			}
+		} else if (e.target.name === 'clientType') {
+			if (e.target.value === 'RENT') {
+				setFormData({
+					...formData,
+					[e.target.name]: e.target.value,
+					saleParty: '',
+					loan: false,
+				});
+			} else {
+				setFormData({
+					...formData,
+					[e.target.name]: e.target.value,
+					rentParty: '',
 				});
 			}
 		} else {
@@ -89,9 +106,9 @@ const AddClient = () => {
 						/>
 					</div>
 
-					<div className='mb-6'>
+					<div className='mb-8'>
 						<label htmlFor='address' className='inline-block mb-2'>
-							Address
+							Requirement
 						</label>
 						<input
 							type='text'
@@ -103,7 +120,28 @@ const AddClient = () => {
 						/>
 					</div>
 
-					<ul className='grid w-full gap-6 grid-cols-2 mb-2'>
+					<div className='mb-6'>
+						<select
+							id='propertyType'
+							name='propertyType'
+							onChange={handleInputChange}
+							value={formData.propertyType}
+							className='bg-[#243b55] rounded w-full px-3 py-1.5 focus:border-none'>
+							<option value=''>Select property type</option>
+							<option value='RESIDENTIAL'>Residential</option>
+							<option value='COMMERCIAL'>Commercial</option>
+							<option value='INDUSTRIAL'>Industrial</option>
+							<option value='OPEN-PLOT'>Open Plot</option>
+							<option value='AGRICULTURAL'>Agricultural</option>
+						</select>
+					</div>
+
+					<ul
+						className={
+							'grid w-full gap-x-6 gap-y-2 grid-cols-2 ' +
+							(formData.clientType === '' ? 'mb-6' : 'mb-2')
+						}>
+						<p className='col-span-2'>Client Type</p>
 						<li>
 							<input
 								type='radio'
@@ -136,75 +174,85 @@ const AddClient = () => {
 						</li>
 					</ul>
 
-					<ul className='grid w-full gap-6 grid-cols-2 mb-6'>
-						<li>
-							<input
-								type='radio'
-								id='renter'
-								name='rentParty'
-								onChange={handleInputChange}
-								value='RENTER'
-								className='peer hidden'
-							/>
-							<label
-								htmlFor='renter'
-								className='inline-flex w-full py-2 px-4 bg-[#243b55] rounded cursor-pointer transition ease-in-out peer-checked:bg-accent  hover:bg-opacity-50 peer-checked:hover:brightness-110'>
-								Renter
-							</label>
-						</li>
-						<li>
-							<input
-								type='radio'
-								id='homeowner'
-								name='rentParty'
-								onChange={handleInputChange}
-								value='HOMEOWNER'
-								className='peer hidden'
-							/>
-							<label
-								htmlFor='homeowner'
-								className='inline-flex w-full py-2 px-4 bg-[#243b55] rounded cursor-pointer transition ease-in-out peer-checked:bg-accent  hover:bg-opacity-50 peer-checked:hover:brightness-110'>
-								Homeowner
-							</label>
-						</li>
-					</ul>
+					{formData.clientType === 'RENT' && (
+						<ul className='grid w-full gap-6 grid-cols-2 mb-6'>
+							<li>
+								<input
+									type='radio'
+									id='renter'
+									name='rentParty'
+									onChange={handleInputChange}
+									value='RENTER'
+									checked={formData.rentParty === 'RENTER'}
+									className='peer hidden'
+								/>
+								<label
+									htmlFor='renter'
+									className='inline-flex w-full py-2 px-4 bg-[#243b55] rounded cursor-pointer transition ease-in-out peer-checked:bg-accent  hover:bg-opacity-50 peer-checked:hover:brightness-110'>
+									Renter
+								</label>
+							</li>
+							<li>
+								<input
+									type='radio'
+									id='homeowner'
+									name='rentParty'
+									onChange={handleInputChange}
+									value='HOMEOWNER'
+									checked={formData.rentParty === 'HOMEOWNER'}
+									className='peer hidden'
+								/>
+								<label
+									htmlFor='homeowner'
+									className='inline-flex w-full py-2 px-4 bg-[#243b55] rounded cursor-pointer transition ease-in-out peer-checked:bg-accent  hover:bg-opacity-50 peer-checked:hover:brightness-110'>
+									Homeowner
+								</label>
+							</li>
+						</ul>
+					)}
+
+					{formData.clientType === 'SALE' && (
+						<ul className='grid w-full gap-6 grid-cols-2 mb-6'>
+							<li>
+								<input
+									type='radio'
+									id='buyer'
+									name='saleParty'
+									onChange={handleInputChange}
+									value='BUYER'
+									checked={formData.saleParty === 'BUYER'}
+									className='peer hidden'
+								/>
+								<label
+									htmlFor='buyer'
+									className='inline-flex w-full py-2 px-4 bg-[#243b55] rounded cursor-pointer transition ease-in-out peer-checked:bg-accent  hover:bg-opacity-50 peer-checked:hover:brightness-110'>
+									Buyer
+								</label>
+							</li>
+							<li>
+								<input
+									type='radio'
+									id='Seller'
+									name='saleParty'
+									onChange={handleInputChange}
+									value='SELLER'
+									checked={formData.saleParty === 'SELLER'}
+									className='peer hidden'
+								/>
+								<label
+									htmlFor='Seller'
+									className='inline-flex w-full py-2 px-4 bg-[#243b55] rounded cursor-pointer transition ease-in-out peer-checked:bg-accent  hover:bg-opacity-50 peer-checked:hover:brightness-110'>
+									Seller
+								</label>
+							</li>
+						</ul>
+					)}
 
 					<ul className='grid w-full gap-6 grid-cols-2 mb-6'>
 						<li>
-							<input
-								type='radio'
-								id='buyer'
-								name='saleParty'
-								onChange={handleInputChange}
-								value='BUYER'
-								className='peer hidden'
-							/>
-							<label
-								htmlFor='buyer'
-								className='inline-flex w-full py-2 px-4 bg-[#243b55] rounded cursor-pointer transition ease-in-out peer-checked:bg-accent  hover:bg-opacity-50 peer-checked:hover:brightness-110'>
-								Buyer
+							<label htmlFor='size' className='inline-block mb-2'>
+								Size
 							</label>
-						</li>
-						<li>
-							<input
-								type='radio'
-								id='Seller'
-								name='saleParty'
-								onChange={handleInputChange}
-								value='SELLER'
-								className='peer hidden'
-							/>
-							<label
-								htmlFor='Seller'
-								className='inline-flex w-full py-2 px-4 bg-[#243b55] rounded cursor-pointer transition ease-in-out peer-checked:bg-accent  hover:bg-opacity-50 peer-checked:hover:brightness-110'>
-								Seller
-							</label>
-						</li>
-					</ul>
-
-					<ul className='grid w-full gap-6 grid-cols-2 mb-6'>
-						<li>
-							<label htmlFor='size'>Size</label>
 							<input
 								type='text'
 								id='size'
@@ -215,7 +263,11 @@ const AddClient = () => {
 							/>
 						</li>
 						<li>
-							<label htmlFor='sq-ft'>Sq. Ft.</label>
+							<label
+								htmlFor='sq-ft'
+								className='inline-block mb-2'>
+								Sq. Ft.
+							</label>
 							<input
 								type='number'
 								id='sq-ft'
@@ -228,7 +280,12 @@ const AddClient = () => {
 					</ul>
 
 					<ul className='grid w-full gap-6 grid-cols-2 mb-6'>
-						<li className='col-span-2'>
+						<li
+							className={
+								formData.saleParty !== 'BUYER'
+									? 'col-span-2'
+									: ''
+							}>
 							<label
 								htmlFor='budget'
 								className='inline-block mb-2'>
@@ -243,23 +300,30 @@ const AddClient = () => {
 								className='block w-full px-3 py-1.5 text-l bg-transparent border border-solid border-gray-300 rounded transition ease-in-out focus:border-accent focus:outline-none'
 							/>
 						</li>
-						{/* when client is buyer then show this */}
-						<li className='flex items-end'>
-							<select
-								id='loan'
-								name='loan'
-								onChange={handleInputChange}
-								// value={formData.loan}
-								className='bg-[#243b55] rounded w-full px-3 py-1.5 focus:border-none'>
-								<option value=''>Select Loan option</option>
-								<option value={true}>Yes</option>
-								<option value={false}>No</option>
-							</select>
-						</li>
+						{formData.saleParty === 'BUYER' && (
+							<li className='flex items-end'>
+								<select
+									id='loan'
+									name='loan'
+									onChange={handleInputChange}
+									// value={formData.loan}
+									className='bg-[#243b55] rounded w-full px-3 py-1.5 focus:border-none'>
+									<option value=''>Select Loan option</option>
+									<option value={true}>Yes</option>
+									<option value={false}>No</option>
+								</select>
+							</li>
+						)}
 					</ul>
 
-					<ul className='grid w-full gap-6 grid-cols-2 mb-6'>
-						<li>
+					<ul className='grid w-full gap-x-6 grid-cols-2 mb-6'>
+						<li
+							className={
+								formData.lead === '' ||
+								formData.lead === 'WALK-IN'
+									? 'col-span-2'
+									: ''
+							}>
 							<select
 								id='lead'
 								name='lead'
@@ -273,27 +337,33 @@ const AddClient = () => {
 							</select>
 						</li>
 						<li>
-							<input
-								type='text'
-								name='leadAgentName'
-								onChange={handleInputChange}
-								value={formData.leadAgentName}
-								placeholder='Agent Name'
-								className='block w-full px-3 py-1.5 text-l bg-transparent border border-solid border-gray-300 rounded transition ease-in-out focus:border-accent focus:outline-none'
-							/>
-							<select
-								id='lead-online'
-								name='leadOnlineName'
-								onChange={handleInputChange}
-								value={formData.leadOnlineName}
-								className='bg-[#243b55] rounded w-full px-3 py-1.5 focus:border-none'>
-								<option value=''>Select online option</option>
-								<option value='UNKNOWN'>Unknown</option>
-								<option value='JUST-DIAL'>Just dial</option>
-								<option value='SQUARE-YARDS'>
-									square yards
-								</option>
-							</select>
+							{formData.lead === 'REFERENCE' && (
+								<input
+									type='text'
+									name='leadAgentName'
+									onChange={handleInputChange}
+									value={formData.leadAgentName}
+									placeholder='Agent Name'
+									className='block w-full px-3 py-1.5 text-l bg-transparent border border-solid border-gray-300 rounded transition ease-in-out focus:border-accent focus:outline-none'
+								/>
+							)}
+							{formData.lead === 'ONLINE' && (
+								<select
+									id='lead-online'
+									name='leadOnlineName'
+									onChange={handleInputChange}
+									value={formData.leadOnlineName}
+									className='bg-[#243b55] rounded w-full px-3 py-1.5 focus:border-none'>
+									<option value=''>
+										Select online option
+									</option>
+									<option value='UNKNOWN'>Unknown</option>
+									<option value='JUST-DIAL'>Just dial</option>
+									<option value='SQUARE-YARDS'>
+										square yards
+									</option>
+								</select>
+							)}
 						</li>
 					</ul>
 
