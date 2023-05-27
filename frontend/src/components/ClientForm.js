29299d05fa9e01b691906872aa9378a941d4b68f
Bhushan Alhat <alhatbhushan@gmail.com>
2023-05-27 14:30:46 +0530

    used update client reducer in clientForm

--- frontend/src/components/ClientForm.js
+++ frontend/src/components/ClientForm.js
@@ -2,11 +2,19 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { addClientToDB, resetStatus } from '../features/ClientsSlice';
+import {
+	addClientToDB,
+	updateClientToDB,
+	resetStatus,
+} from '../features/ClientsSlice';
 import useForm from '../hooks/useValidateForm';
 import ErrorMessage from './ErrorMessage';
 import CONSTANT_LITERALS from '../Constants/Constants';
 import capitalizeFirstLetter from '../utilities/capitalizeFirstLetter';
+import SuccessButton from '../UI/SuccessButton';
+import LoadingButton from '../UI/LoadingButton';
+import SubmitButton from '../UI/SubmitButton';
+import FailureButton from '../UI/FailureButton';
 
 const ClientForm = ({ updateClient }) => {
 	// todo show commas in budget input
@@ -14,13 +22,13 @@ const ClientForm = ({ updateClient }) => {
 	// todo prevent spaces in mobile field
 	// todo display error msg when submit fails
 
-	const { pathname } = useLocation();
+	const { pathname, state: locationState } = useLocation();
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
 
 	const [formIsValid, setFormIsValid] = useState(false);
-	const addClientsStatus = useSelector(
-		(state) => state.clients.addClientsStatus
+	const { addClientsStatus, updateClientsStatus } = useSelector(
+		(state) => state.clients
 	);
 
 	const {
@@ -34,13 +42,13 @@ const ClientForm = ({ updateClient }) => {
 	useEffect(() => {
 		if (
 			pathname === '/updateClient' &&
-			addClientsStatus === CONSTANT_LITERALS.STATUS.SUCCESS
+			updateClientsStatus === CONSTANT_LITERALS.STATUS.SUCCESS
 		) {
 			setTimeout(() => {
 				navigate('/', { replace: true });
 			}, 500);
 		}
-	}, [addClientsStatus, pathname, navigate]);
+	}, [updateClientsStatus, pathname, navigate]);
 
 	// reset form fields only on success
 	useEffect(() => {
@@ -53,13 +61,15 @@ const ClientForm = ({ updateClient }) => {
 	useEffect(() => {
 		if (
 			addClientsStatus === CONSTANT_LITERALS.STATUS.SUCCESS ||
-			addClientsStatus === CONSTANT_LITERALS.STATUS.FAILURE
+			addClientsStatus === CONSTANT_LITERALS.STATUS.FAILURE ||
+			updateClientsStatus === CONSTANT_LITERALS.STATUS.SUCCESS ||
+			updateClientsStatus === CONSTANT_LITERALS.STATUS.FAILURE
 		) {
 			setTimeout(() => {
 				dispatch(resetStatus());
 			}, 2000);
 		}
-	}, [dispatch, addClientsStatus]);
+	}, [dispatch, addClientsStatus, updateClientsStatus]);
 
 	const updateFunc = useCallback(() => {
 		if (updateClient) {
@@ -162,24 +172,33 @@ const ClientForm = ({ updateClient }) => {
 		}
 
 		if (formIsValid) {
-			dispatch(
-				addClientToDB({
-					name: formState.clientName.value.trim(),
-					mobile: formState.mobile.value,
-					address: formState.address.value.trim(),
-					propertyType: formState.propertyType.value,
-					clientType: formState.clientType.value,
-					rentParty: formState.rentParty.value,
-					saleParty: formState.saleParty.value,
-					loan: formState.loan.value,
-					size: formState.size.value,
-					sqft: formState.sqft.value,
-					budget: formState.budget.value,
-					lead: formState.lead.value,
-					leadAgentName: formState.leadAgentName.value.trim(),
-					leadOnlineName: formState.leadOnlineName.value,
-				})
-			);
+			const clientData = {
+				name: formState.clientName.value.trim(),
+				mobile: formState.mobile.value,
+				address: formState.address.value.trim(),
+				propertyType: formState.propertyType.value,
+				clientType: formState.clientType.value,
+				rentParty: formState.rentParty.value,
+				saleParty: formState.saleParty.value,
+				loan: formState.loan.value,
+				size: formState.size.value,
+				sqft: formState.sqft.value,
+				budget: formState.budget.value,
+				lead: formState.lead.value,
+				leadAgentName: formState.leadAgentName.value.trim(),
+				leadOnlineName: formState.leadOnlineName.value,
+			};
+
+			if (pathname === '/updateClient') {
+				dispatch(
+					updateClientToDB({
+						clientId: locationState.client._id,
+						clientData,
+					})
+				);
+			} else {
+				dispatch(addClientToDB(clientData));
+			}
 
 			setFormIsValid(false);
 		}
@@ -678,49 +697,33 @@ const ClientForm = ({ updateClient }) => {
 					</li>
 				</ul>
 
-				{addClientsStatus === '' ? (
-					<button className='w-full bg-transparent border py-2 mt-6 transition ease-in-out hover:border-none hover:bg-accent'>
-						Submit
-					</button>
-				) : addClientsStatus === CONSTANT_LITERALS.STATUS.LOADING ? (
-					<button
-						disabled
-						type='button'
-						className='w-full py-2 mt-12 bg-accent mr-2 flex justify-center'>
-						<div className='inline-flex items-center'>
-							<svg
-								aria-hidden='true'
-								role='status'
-								className='inline w-4 h-4 mr-3 text-white animate-spin'
-								viewBox='0 0 100 101'
-								fill='none'
-								xmlns='http://www.w3.org/2000/svg'>
-								<path
-									d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
-									fill='#E5E7EB'
-								/>
-								<path
-									d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
-									fill='currentColor'
-								/>
-							</svg>
-							<span>Loading...</span>
-						</div>
-					</button>
-				) : addClientsStatus === CONSTANT_LITERALS.STATUS.SUCCESS ? (
-					<button
-						disabled
-						className='w-full py-2 mt-12 bg-accent mr-2 flex justify-center'>
-						Successfully{' '}
-						{pathname === '/updateClient' ? 'Updated' : 'Added'}!
-					</button>
-				) : addClientsStatus === CONSTANT_LITERALS.STATUS.FAILURE ? (
-					<button
-						disabled
-						className='w-full py-2 mt-12 bg-red-400 mr-2 flex justify-center'>
-						Failed!
-					</button>
-				) : null}
+				{pathname !== '/updateClient' &&
+					(addClientsStatus === '' ? (
+						<SubmitButton />
+					) : addClientsStatus ===
+					  CONSTANT_LITERALS.STATUS.LOADING ? (
+						<LoadingButton />
+					) : addClientsStatus ===
+					  CONSTANT_LITERALS.STATUS.SUCCESS ? (
+						<SuccessButton msg='Added' />
+					) : addClientsStatus ===
+					  CONSTANT_LITERALS.STATUS.FAILURE ? (
+						<FailureButton />
+					) : null)}
+
+				{pathname === '/updateClient' &&
+					(updateClientsStatus === '' ? (
+						<SubmitButton />
+					) : updateClientsStatus ===
+					  CONSTANT_LITERALS.STATUS.LOADING ? (
+						<LoadingButton />
+					) : updateClientsStatus ===
+					  CONSTANT_LITERALS.STATUS.SUCCESS ? (
+						<SuccessButton msg='Updated' />
+					) : updateClientsStatus ===
+					  CONSTANT_LITERALS.STATUS.FAILURE ? (
+						<FailureButton />
+					) : null)}
 			</form>
 		</div>
 	);
