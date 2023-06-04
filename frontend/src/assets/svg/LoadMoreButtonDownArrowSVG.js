bd053e4cc34ed50e32d14f6200ce8a3d891dea70
Bhushan Alhat <alhatbhushan@gmail.com>
2023-06-04 15:55:09 +0530

    moved svg to assets/svg as react components

---
+++ frontend/src/assets/svg/LoadMoreButtonDownArrowSVG.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const LoadMoreButtonDownArrowSVG = () => {
+	return (
+		<svg
+			className='inline w-6 h-6 mr-3 text-white animate-bounce'
+			fill='none'
+			stroke='currentColor'
+			viewBox='0 0 24 24'
+			xmlns='http://www.w3.org/2000/svg'>
+			<path
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				strokeWidth='2'
+				d='M19 14l-7 7m0 0l-7-7m7 7V3'></path>
+		</svg>
+	);
+};
+
+export default LoadMoreButtonDownArrowSVG;
--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -6,6 +6,9 @@ import { updateSearch } from '../features/FiltersSlice';
 import { fetchClients } from '../features/ClientsSlice';
 import removeEmpty from '../utilities/removeEmpty';
 import debounce from '../utilities/debounce';
+import FilterSVG from '../assets/svg/FilterSVG';
+import SearchInputMagnifyingSVG from '../assets/svg/SearchInputMagnifyingSVG';
+import SearchButtonMagnifyingSVG from '../assets/svg/SearchButtonMagnifyingSVG';
 
 const SearchBar = ({ setIsFilterActive }) => {
 	const dispatch = useDispatch();
@@ -62,34 +65,12 @@ const SearchBar = ({ setIsFilterActive }) => {
 					type='button'
 					className='inline-flex items-center order-last sm:order-none sm:grow-0 grow py-2.5 px-3 text-sm font-medium bg-transparent border rounded transition ease-in-out hover:border-accent hover:bg-accent'
 					onClick={filterHandler}>
-					<svg
-						className='w-5 h-5 mr-2 -ml-1 text-white'
-						fill='none'
-						stroke='currentColor'
-						viewBox='0 0 24 24'
-						xmlns='http://www.w3.org/2000/svg'>
-						<path
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							// strokeWidth='2'
-							d='M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4'></path>
-					</svg>
+					<FilterSVG />
 					Filter
 				</button>
 				<div className='relative w-full'>
 					<div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
-						<svg
-							aria-hidden='true'
-							className='w-5 h-5 text-gray-500 dark:text-gray-400'
-							fill='currentColor'
-							viewBox='0 0 20 20'
-							xmlns='http://www.w3.org/2000/svg'>
-							<path
-								fillRule='evenodd'
-								d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
-								clipRule='evenodd'
-							/>
-						</svg>
+						<SearchInputMagnifyingSVG />
 					</div>
 					<input
 						type='text'
@@ -105,20 +86,7 @@ const SearchBar = ({ setIsFilterActive }) => {
 					type='submit'
 					onClick={searchHandler}
 					className='inline-flex items-center grow sm:grow-0 py-2.5 px-3 text-sm font-medium bg-transparent border rounded transition ease-in-out hover:border-accent hover:bg-accent'>
-					<svg
-						aria-hidden='true'
-						className='w-5 h-5 mr-2 -ml-1'
-						fill='none'
-						stroke='currentColor'
-						viewBox='0 0 24 24'
-						xmlns='http://www.w3.org/2000/svg'>
-						<path
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							strokeWidth={2}
-							d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
-						/>
-					</svg>
+					<SearchButtonMagnifyingSVG />
 					Search
 				</button>
 			</div>
