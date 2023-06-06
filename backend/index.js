3ae39ab247eebcf6d52b45a2a3b3d483b3e0e31a
Bhushan Alhat <alhatbhushan@gmail.com>
2023-06-06 23:28:46 +0530

    initialized passport and session

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
+const passport = require('passport');
+const expressSession = require('express-session');
 
 const connectDB = require('./Database/database');
 const ClientRoutes = require('./Routes/ClientRoutes');
 
 const app = express();
+
+app.use(
+	expressSession({
+		secret: process.env.COOKIE_SECRET,
+		resave: true,
+		saveUninitialized: true,
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.json());
 app.use(cors());
