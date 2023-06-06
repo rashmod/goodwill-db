ebbb57a5fe07168f547cdbfe557a95aaa13ae42e
Bhushan Alhat <alhatbhushan@gmail.com>
2023-06-06 23:40:31 +0530

    setup auth routes

---
+++ backend/Routes/AuthRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const passport = require('passport');
+const catchAsyncError = require('../Utilities/catchAsyncError');
+const { signup } = require('../Controllers/AuthController');
+
+const router = express.Router();
+
+router.get('/google', passport.authenticate('google', ['profile']));
+
+router.get(
+	'/google/callback',
+	passport.authenticate('google', {
+		successRedirect: process.env.CLIENT_URL,
+		failureRedirect: `${process.env.CLIENT_URL}/signup?failure=true`,
+	})
+);
+
+router.get('/signup', catchAsyncError(signup));
+
+module.exports = router;
