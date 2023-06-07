17985758b855feb29bfda6b5dd37bf484e129a53
Bhushan Alhat <alhatbhushan@gmail.com>
2023-06-07 13:02:50 +0530

    added user signout controller

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -2,8 +2,33 @@
 // @route GET /api/auth/signup
 // @access Public
 module.exports.signup = async (req, res) => {
-	res.status(200).json({
-		success: true,
-		data: req.user, // get the user from session
-	});
+	if (req.user) {
+		res.status(200).json({
+			success: true,
+			data: req.user, // get the user from session
+		});
+	} else {
+		res.status(401).json({
+			success: false,
+			error: 'Authentication Failed',
+			// message: errors.array(),
+		});
+	}
+};
+
+module.exports.signout = async (req, res, next) => {
+	if (req.user) {
+		req.logout((err) => {
+			if (err) return next(err);
+			res.status(200).json({
+				success: true,
+			});
+		});
+	} else {
+		res.status(400).json({
+			success: false,
+			error: 'Bad Request',
+			// message: errors.array(),
+		});
+	}
 };
