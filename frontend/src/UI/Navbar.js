bf4f1309b5b0d0cbdfa90ea270907bda5efa6c42
Bhushan Alhat <alhatbhushan@gmail.com>
2023-06-07 13:11:02 +0530

    added links to signup and signout

--- frontend/src/UI/Navbar.js
+++ frontend/src/UI/Navbar.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { userSignOut } from '../features/UserSlice';
+import CONSTANT_LITERALS from '../Constants/Constants';
 
 const Navbar = () => {
+	const dispatch = useDispatch();
+	const user = useSelector((state) => state.user);
+	const isSignedIn =
+		user.isLoggedIn &&
+		user.id.length > 0 &&
+		user.status === CONSTANT_LITERALS.STATUS.SUCCESS &&
+		user.error === '';
+
 	return (
 		<nav className='flex justify-between container text-sm sm:text-base py-4 px-4 sm:px-0 max-w-xs sm:max-w-lg md:max-w-3xl lg:max-w-5xl mx-auto'>
 			<span>Goodwill DB</span>
@@ -24,15 +35,28 @@ const Navbar = () => {
 						Add Client
 					</NavLink>
 				</li>
-				<li>
-					<NavLink
-						className={({ isActive }) =>
-							isActive ? 'underline underline-offset-4' : ''
-						}
-						to='/signup'>
-						Sign Up
-					</NavLink>
-				</li>
+				{isSignedIn ? (
+					<li>
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'underline underline-offset-4' : ''
+							}
+							onClick={() => dispatch(userSignOut())}
+							to='/signup'>
+							Sign Out
+						</NavLink>
+					</li>
+				) : (
+					<li>
+						<NavLink
+							className={({ isActive }) =>
+								isActive ? 'underline underline-offset-4' : ''
+							}
+							to='/signup'>
+							Sign Up
+						</NavLink>
+					</li>
+				)}
 			</ul>
 		</nav>
 	);
