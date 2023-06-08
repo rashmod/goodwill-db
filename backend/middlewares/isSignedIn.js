1a6553d2620a1d67223007d340111ab70682b11a
Bhushan Alhat <alhatbhushan@gmail.com>
2023-06-08 17:32:11 +0530

    [Backend] added isSignedIn middleware to check if user is signed

---
+++ backend/middlewares/isSignedIn.js
@@ -0,0 +1,10 @@
+module.exports.isSignedIn = (req, res, next) => {
+	if (!req.isAuthenticated()) {
+		return res.status(401).json({
+			success: false,
+			error: 'Authentication Failed',
+			msg: 'Access Restricted: Please Log In',
+		});
+	}
+	next();
+};
